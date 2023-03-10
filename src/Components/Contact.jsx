9298d35a30aff180606c4f8e5d8763fafc1b6281
Mohammad Chreif <cmohamadch@gmail.com>
2023-03-10 15:09:29 +0200

    Fixed bootstrap import

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 const Contact = () => {
   const [formStatus, setFormStatus] = React.useState("Send");
   const onSubmit = (e) => {
@@ -14,31 +14,36 @@ const Contact = () => {
     console.log(conFom);
   };
   return (
-    <div id="contact" className="container mt-5">
-      <h2 className="mb-3">React Contact Form Component Example</h2>
-      <form onSubmit={onSubmit}>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input className="form-control" type="text" id="name" required />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="email">
-            Email
-          </label>
-          <input className="form-control" type="email" id="email" required />
-        </div>
-        <div className="mb-3">
-          <label className="form-label" htmlFor="message">
-            Message
-          </label>
-          <textarea className="form-control" id="message" required />
-        </div>
-        <button className="btn btn-danger" type="submit">
-          {formStatus}
-        </button>
-      </form>
+    <div id="contact" className="container mt-5 p-3">
+      <div className="d-flex justify-content-center flex-column ">
+        <h2 className="d-flex mb-5 justify-content-center">Contact Us</h2>
+        <form
+          className="d-flex justify-content-center flex-column"
+          onSubmit={onSubmit}
+        >
+          <div className="mb-3">
+            <label className="form-label" htmlFor="name">
+              Name
+            </label>
+            <input className="form-control" type="text" id="name" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input className="form-control" type="email" id="email" required />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="message">
+              Message
+            </label>
+            <textarea className="form-control" id="message" required />
+          </div>
+          <button className="mb-5 btn btn-warning " type="submit">
+            {formStatus}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
